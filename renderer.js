6f235e2aed77c226cf1459b660857ceb13ae5beb
Alan <alanscodelog@gmail.com>
2018-05-04 16:17:11 -0300

    Reproduces webview problem.

--- renderer.js
+++ renderer.js
@@ -1,3 +1,25 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+let type = "webview"
+function add_elements() {
+	for (var i = 0; i < 10; i++) {
+		append()
+	}
+}
+function append() {
+	let webview
+	let place
+	webview = document.createElement(type);
+	webview.setAttribute('src', "./plugin/index.html");
+	// webview.setAttribute('src', "about:blank");
+	webview.setAttribute('preload', "./preload.js")
+	place = document.querySelector(".plugins")
+	place.appendChild(webview)
+}
+
+function remove_elements() {
+	document.querySelectorAll(type).forEach(el=> {
+		el.remove()
+	})
+}
+
+document.querySelector(".add").addEventListener("mousedown", add_elements)
+document.querySelector(".remove").addEventListener("mousedown", remove_elements)
